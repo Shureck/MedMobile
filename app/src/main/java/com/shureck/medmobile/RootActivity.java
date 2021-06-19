f331e50378721b25a1232f837511c799c55b094c
Artemiy Matchin <art16ws@gmail.com>
2021-06-19 14:56:24 +0300

    Register device activity UI

--- app/src/main/java/com/shureck/medmobile/RootActivity.java
+++ app/src/main/java/com/shureck/medmobile/RootActivity.java
@@ -2,7 +2,9 @@ package com.shureck.medmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 
 public class RootActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class RootActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+
+        Intent intentTest = new Intent(RootActivity.this, RegisterDeviceActivity.class);
+        startActivity(intentTest);
     }
 }
