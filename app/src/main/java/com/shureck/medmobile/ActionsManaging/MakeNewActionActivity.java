80df1e144de85a8395979c9e673f8061414c2a1a
Artemiy Matchin <art16ws@gmail.com>
2021-06-20 01:29:24 +0300

    Diary and notify activities ready

--- app/src/main/java/com/shureck/medmobile/ActionsManaging/MakeNewActionActivity.java
+++ app/src/main/java/com/shureck/medmobile/ActionsManaging/MakeNewActionActivity.java
@@ -2,15 +2,30 @@ package com.shureck.medmobile.ActionsManaging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.shureck.medmobile.InputData.CheckInputActivity;
 import com.shureck.medmobile.R;
+import com.shureck.medmobile.RootActivity;
+import com.shureck.medmobile.SelectNewActionTypeActivity;
 
 public class MakeNewActionActivity extends AppCompatActivity {
+
+    Button getBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_new_action);
+
+        getBackButton = findViewById(R.id.button_back);
+
+        getBackButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MakeNewActionActivity.this, SelectNewActionTypeActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/com/shureck/medmobile/SetParametersForActionActivity.java
+++ app/src/main/java/com/shureck/medmobile/SetParametersForActionActivity.java
@@ -2,14 +2,19 @@ package com.shureck.medmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.tv.TvContentRating;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.shureck.medmobile.ActionsManaging.MakeNewActionActivity;
 
 public class SetParametersForActionActivity extends AppCompatActivity {
 
     TextView headerView;
     TextView subHeaderView;
+    Button getBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +22,20 @@ public class SetParametersForActionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_parameters_for_action);
 
         String header = getIntent().getStringExtra("HEADER");
-        String subHeader = '(' + getIntent().getStringExtra("SUB_HEADER")  + ')';
+        String subHeader = '(' + getIntent().getStringExtra("SUB_HEADER") + ')';
 
         headerView = findViewById(R.id.set_params_action_header);
         subHeaderView = findViewById(R.id.set_params_action_sub_header);
 
         headerView.setText(header);
         subHeaderView.setText(subHeader);
+
+        getBackButton = findViewById(R.id.button_back);
+
+        getBackButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SetParametersForActionActivity.this, SelectNewActionTypeActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
